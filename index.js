a5090cd21cc16ef44f9afcdfb00fb90ccd10a4e8
WuimmHoften <erickgarciavalenzuela@hotmail.com>
2020-03-04 13:53:55 -0700

    Funcionalidad de Javascript

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
 var elemSendButton = document.getElementById("send_button")
- console.log(elemSendButton);
+console.log(elemSendButton);
 
 const USER_MESSAGE_ELEMENT_ID = "user_message";
 const ERROR_MESSAGE_ELEMENT_CLASS = "error-message";
 
 
- function showMessage(message, isError) {
-        
-        var elemPreviousMessage = document.getElementById("user_message")
+function showMessage(message, isError) {
+       //Borrar mensaje anterior en caso de existir
+       var elemPreviousMessage = document.getElementById("user_message")
 
-        if (elemPreviousMessage) {
-            elemPreviousMessage.parentNode.removeChild(elemPreviousMessage);
-        }
+       if (elemPreviousMessage) {
+           elemPreviousMessage.parentNode.removeChild(elemPreviousMessage);
+       }
 
+       //Crear mensaje de bienvenida / element p
+       var elemMessage = document.createElement("p");
+       elemMessage.innerText = message;
+       elemMessage.id = "user_message";
+       if(isError)
+       {
+           elemMessage.className = "error-message";
+       }
        
-        var elemMessage = document.createElement("p");
-        elemMessage.innerText = message;
-        elemMessage.id = "user_message";
-        if(isError)
-        {
-            elemMessage.className = "error-message";
-        }
-        
-    }
+
+       //Mostrar mensaje de bienvenida
+       var elemMainDiv = document.getElementById("main_div");
+       elemMainDiv.appendChild(elemMessage);
+   }
 elemSendButton.onclick = function () {
-    console.log("Send button clicked")
+   console.log("Send button clicked")
 
-    var elemEmail = document.getElementById("youremail");
-    var elemName = document.getElementById("yourname");
-    var elemMess = document.getElementById("yourmessage");
+   var elemEmail = document.getElementById("youremail");
+   var elemName = document.getElementById("yourname");
+   var elemMess = document.getElementById("yourmessage");
 
-    var youremail = elemEmail.value;
-    var yourname = elemName.value;
-    var yourmessage = elemMess.value;
+   console.log(elemEmail);
+   console.log(elemName);
+   console.log(elemMess);
 
-    if (youremail === "" && yourname === "" && yourmessage === ""){
-        showMessage("Campos de textos vacios", true);
-        
-    }
+   var email = elemEmail.value;
+   var name = elemName.value;
+   var mess = elemMess.value;
+   console.log(email);
+   console.log(name);
+   console.log(mess);
 
+   if (email === "admin" && name === "admin" && mess ==="admin") {
+
+    //Crear mensaje de bienvenida / element p
+    //var elemWelcomeMessage = document.createElement("p");
+    //elemWelcomeMessage.innerText = "Bienvenido " + username;
+
+    //Mostrar mensaje de bienvenida
+    //var elemMainDiv = document.getElementById("main_div");
+    //elemMainDiv.appendChild(elemWelcomeMessage);
+
+    showMessage("Bienvenido " + name);
+
+} else {
+    showMessage("Credenciales incorrectas", true);
 }
 
+if (email === "" && name === "" && mess === ""){
+    showMessage("Campos de textos vacios", true);
+    
+}
 
+}
